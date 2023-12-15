07535760934db56f5ba7f301c3211e2aeee1e5e8
NatalyMit <mitenkova91@icloud.com>
2023-12-15 14:45:12 +0100

    task-3

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const userName = document.querySelector('#name-input');
+
+const spanName = document.querySelector('#name-output');
+
+userName.addEventListener('input', e => {
+  const nameForm = e.target.value.trim();
+  if (userName.value === nameForm) {
+    spanName.textContent = nameForm;
+  } else {
+    spanName.textContent = 'Anonymous';
+  }
+
+  e.preventDefault();
+});
