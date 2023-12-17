0b1a5a69fc71cb8b391838d231964b7a03460286
NatalyMit <mitenkova91@icloud.com>
2023-12-17 20:40:46 +0100

    task - 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,30 +1,41 @@
-'use strict';
 const form = document.querySelector('input');
-const box = document.querySelector('#boxes');
+const boxes = document.querySelector('#boxes');
 const btnCreate = document.querySelector('[data-create]');
 const btnDestroy = document.querySelector('[data-destroy]');
-let elemDivSize = 30;
 
-form.addEventListener('input', e => {
-  e.preventDefault();
-  const inputForm = e.target.value;
-  console.log(inputForm);
-});
 btnCreate.addEventListener('click', e => {
-  e.preventDefault();
-  let randomColor = getRandomHexColor();
-  box.style.width = `${elemDivSize}px`;
-  box.style.height = `${elemDivSize}px`;
-  box.style.backgroundColor = randomColor;
-  console.log(boxDiv);
-});
-function createBoxes(amount) {
-  if (amount == Number && 0 < amount < 100) {
+  let amount;
+  if (form.value > 0 && form.value < 100) {
+    amount = form.value;
+  } else {
+    alert`Please, write the correct condition: '1-100' :blush:`;
+    return;
   }
-}
-function destroyBoxes(params) {}
+  createBoxes(amount);
+});
+btnDestroy.addEventListener('click', е => {
+  deleteBox();
+  form.value = '';
+});
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  deleteBox();
+  let boxSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    let box = document.createElement('div');
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    boxSize += 10;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.append(box);
+  }
+}
+
+function deleteBox() {
+  boxes.innerHTML = ``;
 }
