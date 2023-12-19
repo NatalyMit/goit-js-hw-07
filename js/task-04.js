0a0f2015fa8e8550cce0f6b077b2d31a73cce232
NatalyMit <mitenkova91@icloud.com>
2023-12-19 15:37:47 +0100

    add task - 4

--- js/task-04.js
+++ js/task-04.js
@@ -6,18 +6,14 @@ console.log(userForm);
 userForm.addEventListener('submit', e => {
   e.preventDefault();
   const elems = e.target.elements;
-
+  const data = {
+    email: elems.email.value.trim(),
+    password: elems.password.value.trim(),
+  };
   if (elems.email.value === '' || elems.password.value === '') {
     alert('All form fields must be filled in');
   } else {
     console.log(data);
     userForm.reset();
   }
-
-  const data = {
-    email: elems.email.value.trim(),
-    password: elems.password.value.trim(),
-  };
-  console.log(data);
-  userForm.reset();
 });
